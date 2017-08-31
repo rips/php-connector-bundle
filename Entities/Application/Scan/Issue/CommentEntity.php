15edd0ff6731ba0496b2d9e5ffb207924c33b025
Nick Coffey <ncoffey@ripstech.com>
2017-08-31 12:51:08 -0400

    Add comment entity and hydrator logic

--- Entities/Application/Scan/Issue/CommentEntity.php
+++ Entities/Application/Scan/Issue/CommentEntity.php
@@ -2,6 +2,139 @@
 
 namespace RIPS\ConnectorBundle\Entities\Application\Scan\Issue;
 
+use DateTime;
+use RIPS\ConnectorBundle\Entities\UserEntity;
+use RIPS\ConnectorBundle\Entities\Application\Scan\IssueEntity;
+
 class CommentEntity
 {
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $comment;
+
+    /**
+     * @var DateTime
+     */
+    protected $submission;
+
+    /**
+     * @var UserEntity
+     */
+    protected $createdBy;
+
+    /**
+     * @var IssueEntity
+     */
+    protected $issue;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return void
+     */
+    public function setComment(string $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set submission
+     *
+     * @param DateTime $submission
+     * @return void
+     */
+    public function setSubmission(DateTime $submission)
+    {
+        $this->submission = $submission;
+    }
+
+    /**
+     * Get submission
+     *
+     * @return DateTime
+     */
+    public function getSubmission(): DateTime
+    {
+        return $this->submission;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param UserEntity $createdBy
+     * @return void
+     */
+    public function setCreatedBy(UserEntity $createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return UserEntity
+     */
+    public function getCreatedBy(): UserEntity
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set issue
+     *
+     * @param IssueEntity
+     * @return void
+     */
+    public function setIssue(IssueEntity $issue)
+    {
+        $this->issue = $issue;
+    }
+
+    /**
+     * Get issue
+     *
+     * @return IssueEntity
+     */
+    public function getIssue(): IssueEntity
+    {
+        return $this->issue;
+    }
 }
--- Hydrators/Application/Scan/Issue/CommentHydrator.php
+++ Hydrators/Application/Scan/Issue/CommentHydrator.php
@@ -2,6 +2,47 @@
 
 namespace RIPS\ConnectorBundle\Hydrators\Application\Scan\Issue;
 
+use RIPS\ConnectorBundle\Entities\Application\Scan\Issue\CommentEntity;
+use RIPS\ConnectorBundle\Hydrators\UserHydrator;
+use RIPS\ConnectorBundle\Hydrators\Application\Scan\IssueHydrator;
+
 class CommentHydrator
 {
+    public static function hydrateCollection(array $comments)
+    {
+        $hydrated = [];
+
+        foreach ($comments as $comment) {
+            $hydrated[] = self::hydrate($comment);
+        }
+
+        return $hydrated;
+    }
+
+    public static function hydrate(\stdClass $comment)
+    {
+        $hydrated = new CommentEntity();
+
+        if (isset($comment->id)) {
+            $hydrated->setId($comment->id);
+        }
+
+        if (isset($comment->comment)) {
+            $hydrated->setComment($comment->comment);
+        }
+
+        if (isset($comment->submission)) {
+            $hydrated->setSubmission(new \DateTime($comment->submission));
+        }
+
+        if (isset($comment->created_by)) {
+            $hydrated->setCreatedBy(UserHydrator::hydrate($comment->created_by));
+        }
+
+        if (isset($comment->issue)) {
+            $hydrated->setIssue(IssueHydrator::hydrate($comment->issue));
+        }
+
+        return $hydrated;
+    }
 }
