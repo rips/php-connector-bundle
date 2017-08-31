63b8d706ced6b1a86f56aac276fe7ec61789a048
Nick Coffey <ncoffey@ripstech.com>
2017-08-31 15:03:11 -0400

    Add review type entity and hydrator

---
+++ Hydrators/Application/Scan/Issue/Review/TypeHydrator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RIPS\ConnectorBundle\Hydrators\Application\Scan\Issue\Review;
+
+use DateTime;
+use RIPS\ConnectorBundle\Entities\Application\Scan\Issue\Review\TypeEntity;
+
+class TypeHydrator
+{
+    /**
+     * Hydrate a collection of type objects into a collection of
+     * TypeEntity objects
+     *
+     * @param stdClass[] $type
+     * @return TypeEntity[]
+     */
+    public static function hydrateCollection(array $types)
+    {
+        $hydrated = [];
+
+        foreach ($types as $type) {
+            $hydrated[] = self::hydrate($type);
+        }
+
+        return $hydrated;
+    }
+
+    /**
+     * Hydrate a type object into a TypeEntity object
+     *
+     * @param stdClass $type
+     * @return TypeEntity
+     */
+    public static function hydrate(\stdClass $type)
+    {
+        $hydrated = new TypeEntity();
+
+        if (isset($type->id)) {
+            $hydrated->setId($type->id);
+        }
+
+        if (isset($type->tag)) {
+            $hydrated->setTag($type->tag);
+        }
+
+        if (isset($type->name)) {
+            $hydrated->setName($type->name);
+        }
+
+        return $hydrated;
+    }
+}
