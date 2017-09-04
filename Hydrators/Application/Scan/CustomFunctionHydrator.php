037f4f5ded5160ccc3fadf15362120b3d87760e6
Nick Coffey <ncoffey@ripstech.com>
2017-09-04 14:43:22 -0400

    Custom-Function entity and hydrator fixes
    - Add missing properties
    - Remove return type-hints
    - Fix formatting

--- Hydrators/Application/Scan/CustomFunctionHydrator.php
+++ Hydrators/Application/Scan/CustomFunctionHydrator.php
@@ -3,6 +3,12 @@
 namespace RIPS\ConnectorBundle\Hydrators\Application\Scan;
 
 use RIPS\ConnectorBundle\Entities\Application\Scan\CustomFunctionEntity;
+use RIPS\ConnectorBundle\Hydrators\Application\ScanHydrator;
+use RIPS\ConnectorBundle\Hydrators\Application\Scan\ConcatHydrator;
+use RIPS\ConnectorBundle\Hydrators\Application\Scan\FileHydrator;
+use RIPS\ConnectorBundle\Hydrators\Application\Scan\SinkHydrator;
+use RIPS\ConnectorBundle\Hydrators\Application\Scan\SourceHydrator;
+use RIPS\ConnectorBundle\Hydrators\Application\Scan\CustomClassHydrator;
 
 class CustomFunctionHydrator
 {
@@ -38,16 +44,40 @@ class CustomFunctionHydrator
             $hydrated->setId($function->id);
         }
 
-        if (isset($function->startLine)) {
-            $hydrated->setStartLine($function->startLine);
+        if (isset($function->start_line)) {
+            $hydrated->setStartLine($function->start_line);
         }
 
-        if (isset($function->endLine)) {
-            $hydrated->setEndLine($function->endLine);
+        if (isset($function->end_line)) {
+            $hydrated->setEndLine($function->end_line);
         }
 
         if (isset($function->name)) {
             $hydrated->setName($function->name);
+        }
+
+        if (isset($function->file)) {
+            $hydrated->setFile(FileHydrator($function->file));
+        }
+
+        if (isset($function->class)) {
+            $hydrated->setClass(CustomClassHydrator::hydrate($function->class));
+        }
+
+        if (isset($function->scan)) {
+            $hydrated->setScan(ScanHydrator::hydrate($function->scan));
+        }
+
+        if (isset($function->sources)) {
+            $hydrated->setSources(SourceHydrator::hydrateCollection($function->sources));
+        }
+
+        if (isset($function->sinks)) {
+            $hydrated->setSinks(SinkHydrator::hydrateCollection($function->sinks));
+        }
+
+        if (isset($function->concats)) {
+            $hydrate->setConcats(ConcatHydrator::hydrateCollection($function->concats));
         }
 
         return $hydrated;
