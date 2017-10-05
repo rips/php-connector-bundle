18e1f62e43c5d616d678698c8c93894e3ef2a6e1
Kevin Kloft <kevin.kloft@rub.de>
2017-10-05 13:53:02 +0000

    IssueEntity.php online editiert mit Bitbucket

--- Entities/Application/Scan/IssueEntity.php
+++ Entities/Application/Scan/IssueEntity.php
@@ -5,7 +5,7 @@ namespace RIPS\ConnectorBundle\Entities\Application\Scan;
 use RIPS\ConnectorBundle\Entities\Application\ScanEntity;
 use RIPS\ConnectorBundle\Entities\Application\Scan\ConcatEntity;
 use RIPS\ConnectorBundle\Entities\Application\Scan\SinkEntity;
-use RIPS\ConnectorBundle\Entities\Application\Scan\SourceEntity
+use RIPS\ConnectorBundle\Entities\Application\Scan\SourceEntity;
 use RIPS\ConnectorBundle\Entities\Application\Scan\Issue\CommentEntity;
 use RIPS\ConnectorBundle\Entities\Application\Scan\Issue\SummaryEntity;
 use RIPS\ConnectorBundle\Entities\Application\Scan\Issue\ReviewEntity;
