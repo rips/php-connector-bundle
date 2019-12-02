d3941fb92319f1dfe766cc62888d983e639289a0
Sönke Tölke <stoelke@ripstech.com>
2019-12-02 12:04:50 +0100

    Use new TreeBuilder api

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('rips_connector');
+        $treeBuilder = new TreeBuilder('rips_connector');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
